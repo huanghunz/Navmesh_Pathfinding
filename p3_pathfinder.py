ea26df981ed16e3d5ef1fd200672900e9ac91b24
Jiayu Zeng <huanghunz@hotmail.com>
2015-04-21 18:50:27 -0700

    Cleaned up the codes

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,4 +1,3 @@
-import sys
 import math
 
 try:
@@ -7,13 +6,27 @@ except ImportError:
     import queue as Q
 
 def get_detail_points(prev_detail_pt, box):
-   x_p, y_p = prev_detail_pt
-   x1, x2, y1, y2 = box
-   return ( min(max(x_p, x1), x2), min(max(y_p, y1), y2) )
+  """
+    Based on previous detail point, return the nearest point to next box
+  """
+  x_p, y_p = prev_detail_pt
+  x1, x2, y1, y2 = box
+  return ( min(max(x_p, x1), x2), min(max(y_p, y1), y2) )
+
+def euclidian (p1, p2):
+  x1, y1 = p1
+  x_dest, y_dest = p2
+  return math.sqrt ((x1- x_dest)**2 + (y1- y_dest)**2 ) 
 
 def find_path(src, dest, mesh):
+  """
+    Use awesome bidirectinal A* search with navigation box to find path from two points  
+  """
 
   def get_cost(box, goal):
+    """
+      return the A* cost (total distance and estimation) from goal point to current point
+    """
     curr_box = box
 
     if goal == 'dest':
@@ -26,6 +39,9 @@ def find_path(src, dest, mesh):
     return  distance + heuristic
 
   def get_dist_travelled (box, goal):
+    """
+      return the total distance from goal point to current point
+    """
     dist = 0
     curr_box = box
   
@@ -33,13 +49,13 @@ def find_path(src, dest, mesh):
     
       while forward_prev_box[curr_box]:
         prev_box = forward_prev_box[curr_box]
-        dist += euclidian (detail_points[curr_box], detail_points[ prev_box ])
+        dist += euclidian (detail_points[curr_box], detail_points[prev_box])
         curr_box = prev_box
        
     else:
       while backward_prev_box[curr_box]:
-        prev_box = backward_prev_box [ curr_box ]
-        dist += euclidian (detail_points[curr_box], detail_points [ prev_box])
+        prev_box = backward_prev_box [curr_box]
+        dist += euclidian (detail_points[curr_box], detail_points[prev_box])
         curr_box = prev_box
 
     return dist
@@ -47,7 +63,9 @@ def find_path(src, dest, mesh):
   def get_heuristic (node, dest):
     return euclidian (node, dest)
  
-
+  """
+    ********* Here is where the function starts **********
+  """
   x_src, y_src = src
   x_dest, y_dest = dest
 
@@ -64,13 +82,14 @@ def find_path(src, dest, mesh):
     if src_box and dest_box:
       break
 
+  #early check point
   if not src_box or not dest_box:
     print "Source or destination is not available."
     return ([],[])
 
   elif src_box == dest_box:
     print "They are in the same box."
-    return ( [(src, dest)], [(src_box)] )
+    return ([(src, dest)], [(src_box)])
 
   
   visited_boxes = []
@@ -82,12 +101,12 @@ def find_path(src, dest, mesh):
   detail_points[src_box] = src
   detail_points[dest_box] = dest
 
-  forward_prev_box = {}
+  forward_prev_box = {} # key: a box (x1, x2, y1, y2), value: the parent box
   backward_prev_box = {}
   forward_prev_box[src_box] = None
   backward_prev_box[dest_box] = None
 
-  distance_from_src = {}
+  distance_from_src = {} # key: a box (x1, x2, y1, y2), value: distant from target point
   distance_from_dest = {}
   distance_from_src[src_box] = 0
   distance_from_dest[dest_box] = 0
@@ -96,31 +115,30 @@ def find_path(src, dest, mesh):
   priorityQueue.put ( ( get_cost(src_box, "dest"), src_box, "dest") )
   priorityQueue.put ( ( get_cost(dest_box, "src"), dest_box, "src") )
 
-
   found_path = False
-  mid_box = None
+  connection_box = None
 
   while priorityQueue :
-    cost, box, goal =  priorityQueue.get()
+    cost, box, goal = priorityQueue.get()
    
     if box == dest_box and goal == 'dest' :
       found_path = True
-      mid_box = dest_box
+      connection_box = dest_box
       break
 
     if box == src_box and goal == 'src':
       found_path =True
-      mid_box = src_box
+      connection_box = src_box
       break
 
     if goal == 'dest' and box in backward_prev_box.keys():
       found_path = True
-      mid_box = box
+      connection_box = box
       break
 
     if goal == 'src' and box in forward_prev_box.keys():
       found_path = True
-      mid_box = box
+      connection_box = box
       break
     
     this_prev = {}
@@ -139,8 +157,6 @@ def find_path(src, dest, mesh):
 
         detail_points[adj_box] = get_detail_points (detail_points[box], adj_box)
       
-        #this_prev[adj_box] = box # update that this box is visited
-
         if goal == "dest":
           forward_prev_box[adj_box] = box
           distance_from_src[adj_box] = get_dist_travelled(adj_box, goal)
@@ -149,7 +165,7 @@ def find_path(src, dest, mesh):
           distance_from_dest[adj_box] = get_dist_travelled(adj_box, goal)
         
         visited_boxes.append((adj_box))
-        priorityQueue.put( (get_cost (adj_box, goal), adj_box, goal) )
+        priorityQueue.put( (get_cost(adj_box, goal), adj_box, goal) )
 
   if not found_path:
     print "No Path!"
@@ -159,73 +175,50 @@ def find_path(src, dest, mesh):
     #return ([],[]);
     path = []
 
-    if mid_box == src_box: 
-      curr_box = mid_box
+    if connection_box == src_box: 
+      curr_box = connection_box
 
       while backward_prev_box[curr_box]:
         prev_box = backward_prev_box[curr_box]
         path.append( ( detail_points[curr_box], detail_points[prev_box] )) 
         curr_box = prev_box
 
-      path.append ((src, detail_points[mid_box]))
+      path.append ((src, detail_points[connection_box]))
 
-    elif mid_box == dest_box:
-      curr_box = mid_box
+    elif connection_box == dest_box:
+      curr_box = connection_box
       while forward_prev_box[curr_box]:
         prev_box = forward_prev_box[curr_box]
         path.append( ( detail_points[curr_box], detail_points[prev_box] )) 
         curr_box = prev_box
 
-      path.append ((dest, detail_points[mid_box]))
+      path.append ((dest, detail_points[connection_box]))
 
     else : # they meet at somewhere in the middle
-      connectionBox = mid_box
-      #print "stop"
-      #return ([],[])
-      # curr_box = mid_box
-      # while forward_prev_box[curr_box]:
-      #   prev_box = forward_prev_box[curr_box]
-      #   path.append( ( detail_points[curr_box], detail_points[prev_box] )) 
-      #   curr_box = prev_box
+  
+      box_from_src = forward_prev_box[connection_box]
+      box_from_dest = backward_prev_box[connection_box]
+      point_close_to_src = detail_points[ box_from_src ]
+      point_close_to_dest = detail_points [ box_from_dest]
 
-      # curr_box = mid_box
-      # while backward_prev_box[curr_box]:
-      #   prev_box = backward_prev_box[curr_box]
-      #   path.append( ( detail_points[curr_box], detail_points[prev_box] )) 
-      #   curr_box = prev_box
-      d_1 = forward_prev_box[connectionBox]
-      d_2 = backward_prev_box[connectionBox]
       # calculating the two nearests points of the connection box from their parents' detail point
-      x_p_f, y_p_f = detail_points[d_1]
-      x_p_b, y_p_b = detail_points[d_2]
-      x1, x2, y1, y2 = connectionBox
-      c_detail_forward = ( min(max(x_p_f, x1), x2), min(max(y_p_f, y1), y2) )
-      c_detail_backward = ( min(max(x_p_b, x1), x2), min(max(y_p_b, y1), y2) )
+      c_detail_forward = get_detail_points( point_close_to_src, connection_box)
+      c_detail_backward = get_detail_points( point_close_to_dest, connection_box)
 
       # making line segments 
       path.append((c_detail_forward, c_detail_backward))
-      path.append((c_detail_forward, detail_points[forward_prev_box[connectionBox]]))
-      path.append((c_detail_backward, detail_points[backward_prev_box[connectionBox]]))
+      path.append((c_detail_forward, detail_points[forward_prev_box[connection_box]]))
+      path.append((c_detail_backward, detail_points[backward_prev_box[connection_box]]))
 
-      box = forward_prev_box[connectionBox]
+      box = forward_prev_box[connection_box]
       while forward_prev_box[box] :
         path.append( ( detail_points[box], detail_points[forward_prev_box[box]] )) 
         box = forward_prev_box[box]
 
-      box = backward_prev_box[connectionBox]
+      box = backward_prev_box[connection_box]
       while backward_prev_box[box] :
         path.append( ( detail_points[box], detail_points[backward_prev_box[box]] )) 
         box = backward_prev_box[box]
 
    
     return ( path, visited_boxes )
-
-def euclidian (p1, p2):
-  x1, y1,  = p1
-  x_dest, y_dest = p2
-  return math.sqrt ( (x1- x_dest)**2 +  (y1- y_dest)**2 ) 
-
-
-
-
-
